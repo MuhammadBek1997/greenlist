248074276c2c31a670cb768e37bd14cca400647e
MuhammadBek97 <allayevmuhammad8@gmail.com>
2026-01-08 15:41:32 +0500

    Header for test

---
+++ src/pages/Header.jsx
@@ -0,0 +1,13 @@
+import '../styles/Header.css'
+
+const Header = ()=>{
+    return (
+        <>
+            <h2>
+                Hello
+            </h2>
+        </>
+    )
+}
+
+export default Header
