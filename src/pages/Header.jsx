558c79996d9373d414ca1d96dd2fede3014c3e7c
shohruhxojakeldiyev-source <shohruhxojakeldiyev@gmail.com>
2026-01-09 19:42:11 +0500

    section has done

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,12 +1,38 @@
 import '../styles/Header.css'
 
-const Header = ()=>{
+
+const Header = () => {
     return (
-        <>
-            <h2>
-                Hello
-            </h2>
-        </>
+        <div className='holiday'>
+
+
+            <div className='holiday-img'>
+
+                <img src="botir.png" alt="" />
+            </div>
+
+
+
+
+            <div className='text'>
+
+            <h2>Celebrate <br /> achievements</h2>
+
+            <p>
+                Greenlist celebrates your employee’s waste <br /> reduction achievements
+                by giving custom <br /> designed badges so that they feel proud of the <br /> quantifiable difference they are making towards <br /> the plastic pollution crisis.
+            </p>
+
+            </div>
+
+
+
+
+
+
+
+
+        </div>
     )
 }
 
