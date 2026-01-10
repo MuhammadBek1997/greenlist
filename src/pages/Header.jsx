2ff42b3e2e382afa5b5efa97aac17a76d7a53b64
bekzodjalolov <jalolovbekzod302@gmail.com>
2026-01-10 15:13:16 +0500

    header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,12 +1,52 @@
 import '../styles/Header.css'
 
-const Header = ()=>{
+const Header = () => {
     return (
-        <>
-            <h2>
-                Hello
-            </h2>
-        </>
+      
+            <div className='header'>
+                <div className='nav-logo'>
+                    <h1>
+                        <img src="/images/Frame (8).png" alt="" />
+                    </h1>
+                    <div className='nav-list'>
+                        <h2>
+                            Home
+                        </h2>
+                        <h3>
+                            Pricing
+                        </h3>
+                        <h4>
+                            Support
+                        </h4>
+                    </div>
+                </div>
+                <div className='hero-list'>
+                    <h3>
+                        Green Revolution?
+                    </h3>
+                    <h4>
+                        It’sa team  effort.
+                    </h4>
+                    <div className='hero-logo'>
+                        <img src="/images/Rectangle (7).png" alt="" />
+                    </div>
+                    <div className='hero-logo-png'>
+                        <img src="/images/salat.png" alt="" />
+                    </div>
+                    <div className='hero-logo-npm'>
+                        <img src="/images/logo.png" alt="" />
+
+                    </div>
+                </div>
+                <div className='hero-logo-nmp'>
+                    <img src="/images/Frame (12).png" alt="" />
+                </div>
+                <div className='hero-logo-pmn'>
+                    <img src="/images/Rectangle (8).png" alt="" />
+                </div>
+            </div>
+   
+
     )
 }
 
