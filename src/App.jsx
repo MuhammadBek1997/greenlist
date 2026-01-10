34b7462dc1158d6939b68197d41141249edb4a8c
botir <botirberdaliyev@gmail.com>
2026-01-10 15:11:50 +0500

    footer has done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import Header from './pages/Header'
+import Footer from './pages/Footer'
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <Footer />
     </>
   )
 }
