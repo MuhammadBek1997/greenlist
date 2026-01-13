a34d896bee76663abaf943dafddcd033ddf8d353
bekzodjalolov <jalolovbekzod302@gmail.com>
2026-01-13 13:56:37 +0500

    header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,50 +2,37 @@ import '../styles/Header.css'
 
 const Header = () => {
     return (
-      
-            <div className='header'>
+
+        <div className='header'>
+            <div className='nav'>
                 <div className='nav-logo'>
-                    <h1>
-                        <img src="/images/Frame (8).png" alt="" />
-                    </h1>
-                    <div className='nav-list'>
-                        <h2>
-                            Home
-                        </h2>
-                        <h3>
-                            Pricing
-                        </h3>
-                        <h4>
-                            Support
-                        </h4>
-                    </div>
+                    <img src="/images/Frame (8).png" alt="" />
                 </div>
-                <div className='hero-list'>
+                <div className='nav-list'>
+                    <h2>
+                        Home
+                    </h2>
                     <h3>
-                        Green Revolution?
+                        Pricing
                     </h3>
                     <h4>
-                        It’sa team  effort.
+                        Support
                     </h4>
-                    <div className='hero-logo'>
-                        <img src="/images/Rectangle (7).png" alt="" />
-                    </div>
-                    <div className='hero-logo-png'>
-                        <img src="/images/salat.png" alt="" />
-                    </div>
-                    <div className='hero-logo-npm'>
-                        <img src="/images/logo.png" alt="" />
-
-                    </div>
-                </div>
-                <div className='hero-logo-nmp'>
-                    <img src="/images/Frame (12).png" alt="" />
-                </div>
-                <div className='hero-logo-pmn'>
-                    <img src="/images/Rectangle (8).png" alt="" />
                 </div>
             </div>
-   
+            <div className='hero-list'>
+                <h3>
+                    Green Revolution?
+                </h3>
+                <h4>
+                    It’sa team  effort.
+                </h4>
+            </div>
+            <div className='hero-logo-pn'>
+             <img src="/images/logo.png" alt="" />
+            </div>
+        </div>
+     
 
     )
 }
